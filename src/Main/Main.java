e43a9ea531ab397fd44d36249bde395b21f917c0
Dvleu <L21020400@veracruz.tecnm.mx>
2024-05-28 23:03:14 -0600

    Initial commit

---
+++ src/Main/Main.java
@@ -0,0 +1,28 @@
+package Main;
+
+import Analizador.ParseException;
+import Analizador.Sintaxis;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+
+public class Main {
+    public static void main(String[] args) {
+        String filePath = "C:\\Users\\chapu\\IdeaProjects\\ParserFinalF\\src\\Main\\test.rs";
+
+        try {
+            InputStream inputStream = new FileInputStream(filePath);
+            Sintaxis parser = new Sintaxis(inputStream);
+
+            try {
+                parser.Program();
+                System.out.println("La sintaxis del archivo es correcta.");
+            } catch (ParseException e) {
+                System.out.println("Error de sintaxis: " + e.getMessage());
+            }
+        } catch (FileNotFoundException e) {
+            System.out.println("El archivo no se encontró");
+        }
+    }
+}
---
+++ src/Main/test.rs
@@ -0,0 +1,9 @@
+fn main() {
+    let x: i32 = 123;
+    let z: f64 = 3.1415;
+    let es_verdadero: bool = true;
+    fn tacos() {
+        let x2: i32 = 123123;
+        let y2: String = "sdfsdf";
+    }
+}
