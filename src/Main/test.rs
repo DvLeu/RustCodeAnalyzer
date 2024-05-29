fe54a54bee1fa9ccaed5e72a84ea676fd9937b2e
Dvleu <L21020400@veracruz.tecnm.mx>
2024-05-28 23:07:08 -0600

    Parser Terminado al parecer.

--- src/Main/test.rs
+++ src/Main/test.rs
@@ -1,9 +1,19 @@
 fn main() {
-    let x: i32 = 123;
-    let z: f64 = 3.1415;
-    let es_verdadero: bool = true;
-    fn tacos() {
-        let x2: i32 = 123123;
-        let y2: String = "sdfsdf";
+    let x: i32 = 10;
+    let y: f64 = 20.5;
+    let is_true: bool = true;
+
+    if x < 20 {
+        println!("x is less than 20");
+    } else {
+        println!("x is greater than or equal to 20");
     }
+
+    print_values(x, y, is_true);
+}
+
+fn print_values(a: i32, b: f64, c: bool) {
+    println!("Integer value: {}", a);
+    println!("Float value: {}", b);
+    println!("Boolean value: {}", c);
 }
