19e4d0e7ad1c2728365f9f3e129b572e4d331ef4
Dvleu <L21020400@veracruz.tecnm.mx>
2024-05-29 21:30:01 -0600

    El proyecto hasta ahora deja de tener errores al llamar fucniones, hacer return y no tiene problemas ni con : - IF - ELSEIF - FOR - WHILE

--- src/Main/test.rs
+++ src/Main/test.rs
@@ -10,3 +10,33 @@ fn main() {
     }
     println!("La suma de los números del 1 al 10 es: {}", sum);
 }
+let number = 10;
+
+if number < 0 {
+    println!("The number is negative");
+} else if number == 0 {
+    println!("The number is zero");
+} else {
+    println!("The number is positive");
+}
+fn add(a: i32, b: i32) -> i32 {
+    return a + b;
+    }
+for i in 0..10 {
+    println!("i is at {}", i);
+}
+fn main() {
+    /* Este es un comentario multilínea
+       que abarca varias líneas
+       de código en Rust */
+
+    let x = 10; /* También puedes incluir comentarios multilínea
+                   al lado del código */
+
+    /* Los comentarios multilínea
+       pueden ser anidados
+       /* como este */
+       /*dentro de otros */
+
+    println!("El valor de x es: {}", x);
+}
--- src/Main/test3.rs
+++
@@ -1,9 +0,0 @@
-let number = 10;
-
-if number < 0 {
-    println!("The number is negative");
-} else if number == 0 {
-    println!("The number is zero");
-} else {
-    println!("The number is positive");
-}
